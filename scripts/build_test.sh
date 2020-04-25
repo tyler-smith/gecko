3ba4a9bc511aff0e3b0880f85417958abf9cdea3
Tyler Smith <tylersmith.me@gmail.com>
2020-04-24 17:53:47 -0700

    CI: Run golangci-lint before tests.

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -5,4 +5,7 @@
 SRC_DIR="$(dirname "${BASH_SOURCE[0]}")"
 source "$SRC_DIR/env.sh"
 
+which golangci-lint || (curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.25.0)
+golangci-lint run
+
 go test -race -coverprofile=coverage.out -covermode=atomic ./...
